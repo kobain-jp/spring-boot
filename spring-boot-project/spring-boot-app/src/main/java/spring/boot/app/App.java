26c0ad951d9bddf81467b8832a600e321687a141
yoshifumi <yoshifumi@kobain.local>
2020-06-14 17:19:25 +0900

    add sample app package

---
+++ spring-boot-project/spring-boot-app/src/main/java/spring/boot/app/App.java
@@ -0,0 +1,20 @@
+package spring.boot.app;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@SpringBootApplication
+public class App {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
